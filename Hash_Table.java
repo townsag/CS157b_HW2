5e1706fd7285d497d49f1f667cd257ad8d01b407
townsag <townsag@gmail.com>
2024-03-02 15:31:30 -0800

    added the partitioned hash function to the hash table class. Added a gitignore for consistent development across environments

--- Hash_Table.java
+++ Hash_Table.java
@@ -1,2 +1,68 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
 public class Hash_Table {
+    private int numAttributes;
+    private List<Integer> columnHashRanges;
+    private String tableName;
+
+    public Hash_Table(List<Integer> cHR, String name) {
+        this.columnHashRanges = cHR;
+        this.numAttributes = cHR.size();
+        this.tableName = name;
+    }
+
+    public int partitionedHash(List<String> attributes) throws IllegalArgumentException {
+        if (attributes.size() != numAttributes) {
+            throw new IllegalArgumentException("the number of attributes sent to the partitioned hash" +
+                    "function does not match the number of attributes in the table");
+        }
+
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+
+            int runningHashValue = 0b00;
+            for (int i = 0; i < this.numAttributes; i++) {
+                md.update(attributes.get(i).getBytes());
+                int hashResult = bytesToInt(md.digest());
+                int bitmask = (1 << columnHashRanges.get(i).intValue()) - 1;
+                // System.out.println("i: " + Integer.toString(i) + ", running hash: " +
+                // Integer.toBinaryString(runningHashValue));
+                // System.out.println("md5 hash result: " + Integer.toBinaryString(hashResult));
+                // System.out.println("bitmask: " + Integer.toBinaryString(bitmask));
+                runningHashValue <<= columnHashRanges.get(i).intValue();
+                // System.out.println("running hash After shift: " +
+                // Integer.toBinaryString(runningHashValue));
+                runningHashValue |= (hashResult & bitmask);
+                // System.out.println("running hash After bitwise or: " +
+                // Integer.toBinaryString(runningHashValue) + "\n");
+                md.reset();
+            }
+            return runningHashValue;
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("error in partitionedHash function");
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return -1;
+    }
+
+    private int bytesToInt(byte[] bytes) {
+        int result = 0;
+        // the math.min ensures that the byte array fits in a normal java int
+        for (int i = 0; i < Math.min(bytes.length, 4); i++) {
+            // 8 bits in a byte
+            result <<= 8;
+            // the |= is like doing addition, the lowest 8 bts of the int result are all 0
+            // so
+            // we are or-ing them with the bits of this byte
+            // the (bytes[i] & 0xff) is a bitwise and that ensures we only get the 8 least
+            // significant bits
+            // of the byte, dont know if its necessary
+            result |= (bytes[i] & 0xff);
+        }
+        return result;
+    }
 }
--- PHTManager.java
+++ PHTManager.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
 
 class PHTManager {
-    public static void main(String args[]) {
-        System.out.print("Hello world\n");
+    public static void main(String[] args) {
+        List<Integer> tempInput = Arrays.asList(3, 4, 5);
+        Hash_Table test = new Hash_Table(tempInput, "temp");
+        test.partitionedHash(Arrays.asList("red", "apple", "sunday"));
     }
 }
