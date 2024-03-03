f044c42f34c2ff72d6e79f2c3e79225b03ce0c3e
Peter Guilhamet <peterguilhametjr@gmail.com>
2024-03-02 23:52:57 -0800

    Added a parsing function called parse_instructions that reads from the instruction file one line at a time

--- PHTManager.java
+++ PHTManager.java
@@ -1,6 +1,18 @@
 
+
 class PHTManager {
+
     public static void main(String args[]) {
-        System.out.print("Hello world\n");
+        if (args.length != 2){
+            System.out.println("Please use this format: \njava PHTManager test.sqlite instructions.txt");
+            System.exit(1);
+        }
+
+        Database db = new Database(args[0], args[1]);
+        db.initial_tables();
+        db.parse_instructions(args[1]);
+
     }
+
+
 }
